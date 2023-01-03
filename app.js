3827e9a4015babd5fe1ba414d7df392326bffc8e
Ryu <ryu@realtime.tw>
2023-01-03 08:08:58 +0800

    update: handling error status

--- app.js
+++ app.js
@@ -56,6 +56,15 @@ app.use(
     schema: graphqlSchema,
     rootValue: graphqlResolvers,
     graphiql: true,
+    formatError(err) {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = err.originalError.data;
+      const message = err.message || "An error occurred.";
+      const code = err.originalError.code || 500;
+      return { message, data, status: code };
+    },
   })
 );
 
